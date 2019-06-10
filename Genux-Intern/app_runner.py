5f4dec24aaa033b0bc2c506e587285b2a5cd588d
Samuel Soares <42000522+getsam@users.noreply.github.com>
2019-06-10 13:53:11 -0300

    Update app_runner.py

--- Genux-Intern/app_runner.py
+++ Genux-Intern/app_runner.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 api_key = 'b542def189a74fa5d4b10bd9c35325026d18fedf94e1662863ddb5985c3ebb66'
 url = '''https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,DASH&tsyms=BTC,USD,EUR&
-api_key='''.format(api_key)
+api_key={}'''.format(api_key)
 
 
 #OPÇÕES = BTC,ETH,DASH
